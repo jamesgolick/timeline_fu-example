f6b5741eabc8e815f7f34fa45d329c0bf9c9459e
François Beausoleil <francois@teksol.info>
2009-02-25 10:21:21 -0500

    Made the relationship tests pass

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,7 @@
 class Relationship < ActiveRecord::Base
+  belongs_to :person
+  belongs_to :friend, :class_name => "Person"
+
+  validates_presence_of :person_id, :friend_id
+  attr_accessible :person, :friend
 end
