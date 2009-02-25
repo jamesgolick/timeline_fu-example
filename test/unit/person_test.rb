b6253e1d55e022c351c1b93b13574653578b9da0
François Beausoleil <francois@teksol.info>
2009-02-25 10:17:46 -0500

    updated PersonTest to have good attr_accessible values

--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -4,7 +4,7 @@ class PersonTest < ActiveSupport::TestCase
   should_have_valid_fixtures
   should_require_attributes :name
   should_allow_mass_assignment_of :name
-  should_not_allow_mass_assignment_of :created_at, :updated_at
+  should_not_allow_mass_assignment_of :relationships, :friends, :relationship_ids, :friend_ids
   should_have_many :relationships
   should_have_many :friends
 end
