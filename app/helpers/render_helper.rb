e1f6ce188e492a79a4233105f4f63d33768312ce
François Beausoleil <francois@teksol.info>
2009-02-25 15:37:10 -0500

    Implemented the timeline views

--- app/helpers/render_helper.rb
+++ app/helpers/render_helper.rb
@@ -1,7 +1,11 @@
 module RenderHelper
   def render_timeline(events)
     events.map do |event|
-      render(:partial => "timeline_events/#{event.event_type}", :object => event)
+      render(:partial => "timeline_events/#{event.event_type}", :locals => {:event => event})
     end.join
+  end
+
+  def render_timestamp(time)
+    content_tag(:span, time.to_s(:short), :title => time.to_s(:db), :class => "timestamp")
   end
 end
