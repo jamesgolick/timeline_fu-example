60d8b0ee0027f878899c485c6293f4a37035939a
François Beausoleil <francois@teksol.info>
2009-02-25 13:34:51 -0500

    Set secondary subject to be self for relationships

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -5,8 +5,8 @@ class Relationship < ActiveRecord::Base
   validates_presence_of :person_id, :friend_id
   attr_accessible :person, :friend
 
-  fires :friended, :on => :create, :actor => :person, :subject => :friend
-  fires :unfriended, :on => :destroy, :actor => :person, :subject => :friend
+  fires :friended, :on => :create, :actor => :person, :subject => :friend, :secondary_subject => :self
+  fires :unfriended, :on => :destroy, :actor => :person, :subject => :friend, :secondary_subject => :self
 
   named_scope :after, lambda {|cutoff_at| {:conditions => {:created_at => (cutoff_at.utc .. Time.now.utc)}}}
 end
