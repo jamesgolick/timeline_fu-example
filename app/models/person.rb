69710d0b60a6fc741391e3cf59eeeb0e198179f6
François Beausoleil <francois@teksol.info>
2009-02-25 14:10:45 -0500

    Implemented recent events where I'm that subject and where my friends are the actors

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,5 +5,11 @@ class Person < ActiveRecord::Base
   has_many :relationships
   has_many :friends, :through => :relationships
 
-  has_many :recent_events, :as => :subject, :class_name => "TimelineEvent", :order => "timeline_events.created_at DESC"
+  RecentEventsCondition = '(subject_id = #{id} AND subject_type = \'Person\')
+                          OR (actor_type = \'Person\' AND actor_id IN (SELECT friend_id FROM relationships WHERE relationships.person_id = #{id}))'
+  has_many :recent_events,  :class_name => "TimelineEvent", :order => "timeline_events.created_at DESC",
+                            :finder_sql => 'SELECT timeline_events.* FROM timeline_events
+                                            WHERE ' + RecentEventsCondition + ' ORDER BY timeline_events.created_at DESC',
+                            :counter_sql => 'SELECT COUNT(*) FROM timeline_events WHERE ' + RecentEventsCondition
+
 end
