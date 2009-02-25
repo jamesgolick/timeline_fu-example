8897decdda5499b1f835f71840425fd7e65914a4
François Beausoleil <francois@teksol.info>
2009-02-25 13:38:28 -0500

    Added #recent_events is empty? test

--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -7,4 +7,14 @@ class PersonTest < ActiveSupport::TestCase
   should_not_allow_mass_assignment_of :relationships, :friends, :relationship_ids, :friend_ids
   should_have_many :relationships
   should_have_many :friends
+
+  context "A new person" do
+    setup do
+      @person = Person.create!(:name => "Mat")
+    end
+
+    should "have an empty #recent_events feed" do
+      assert @person.recent_events.empty?
+    end
+  end
 end
