42a495cc767e941918a46050cce9c247070488d0
François Beausoleil <francois@teksol.info>
2009-02-26 10:05:42 -0500

    Propagated changes made to the article back to the example app

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,12 +5,19 @@ class Person < ActiveRecord::Base
   has_many :relationships
   has_many :friends, :through => :relationships
 
-  RecentEventsCondition = '(subject_id = #{id} AND subject_type = \'Person\')
-                          OR (actor_type = \'Person\' AND actor_id IN (SELECT friend_id FROM relationships WHERE relationships.person_id = #{id}))'
-  has_many :recent_events,  :class_name => "TimelineEvent", :order => "timeline_events.created_at DESC",
-                            :finder_sql => 'SELECT timeline_events.* FROM timeline_events
-                                            WHERE ' + RecentEventsCondition + ' ORDER BY timeline_events.created_at DESC',
-                            :counter_sql => 'SELECT COUNT(*) FROM timeline_events WHERE ' + RecentEventsCondition
+  RECENT_EVENTS_CONDITION = 
+    '(subject_id = #{id} AND subject_type = \'Person\')
+    OR (actor_type = \'Person\'
+    AND actor_id IN (SELECT friend_id
+                    FROM relationships
+                    WHERE relationships.person_id = #{id}))'
+    has_many :recent_events,
+      :class_name  => "TimelineEvent",
+      :finder_sql  => 'SELECT timeline_events.* FROM timeline_events
+                      WHERE ' + RECENT_EVENTS_CONDITION + '
+                      ORDER BY timeline_events.created_at DESC',
+      :counter_sql => "SELECT COUNT(*) FROM timeline_events
+                      WHERE " + RECENT_EVENTS_CONDITION
 
   def to_s
     name
